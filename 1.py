180406269fbccbfe4b4e74eb2271824ae848ea0c
kanamycine <jinyeong5320@gmail.com>
2020-09-01 13:20:36 +0900

    python100Q :  1-10

---
+++ 1.py
@@ -0,0 +1,9 @@
+#list의 삭제
+nums = [ 100, 200, 300, 400, 500]
+#del nums[4] 
+#del nums[3]
+#nums.pop()
+#nums.pop()
+nums.remove(400)
+nums.remove(500)
+print(nums)
---
+++ 10.py
@@ -0,0 +1,4 @@
+tc = int(input())
+
+for i in range(tc):
+    print(" "*(tc-i) + "*"*(i+1) + "*"*i)
